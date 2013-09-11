3d926280bb18c7a02b003cddd180135b29f31327
Aj DeFano <ajdefano@gmail.com>
2013-09-11 07:10:28 -0700

    integrated Parse SDK
    runs test push notification

--- src/com/company/weewants/MainActivity.java
+++ src/com/company/weewants/MainActivity.java
@@ -7,16 +7,30 @@ import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //removes title
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //configures go button
         ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
         imageButton1.setOnClickListener(goHandler);
+        // parse sdk initiliaze
+        Parse.initialize(this, "FQEHCb7g6J0pPKokLMdYfyfDgG9CP1gkR9tgEhms", "FIokZyK7sf7vRmpoybJ8ZQeA5WkCQKj6LWOz8PbK");
+        //specify default activity to handle push notifications
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        //tracking
+        ParseAnalytics.trackAppOpened(getIntent());
+
     }
 
 
